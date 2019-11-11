344eb992c75a8bbf64040d5d22a9e7f72ef178f3
Rebecca Oei <e0322808@u.nus.edu>
2019-11-11 18:05:46 +0800

    Add in reset for personList and update PersonListTest code

--- src/test/java/eggventory/logic/commands/add/AddPersonCommandTest.java
+++ src/test/java/eggventory/logic/commands/add/AddPersonCommandTest.java
@@ -4,22 +4,44 @@ import eggventory.commons.enums.CommandType;
 import eggventory.commons.exceptions.BadInputException;
 import eggventory.model.PersonList;
 import eggventory.model.StockList;
+import eggventory.model.loans.Person;
 import eggventory.storage.Storage;
 import eggventory.stubs.StorageStub;
 import eggventory.stubs.UiStub;
 import eggventory.ui.Ui;
+import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
+
+import java.util.ArrayList;
 
 import static org.junit.jupiter.api.Assertions.assertDoesNotThrow;
 import static org.junit.jupiter.api.Assertions.assertThrows;
 
+//@@author cyanoei
 
 class AddPersonCommandTest {
 
     private StockList testStockList = new StockList();
     private Ui testCli = new UiStub();
     private Storage testStorage = new StorageStub();
-    private PersonList personList = new PersonList();
+
+    @BeforeEach
+    void resetPersonList() throws BadInputException {
+        if (PersonList.getSize() == 0) {
+            return;
+        }
+
+        ArrayList<Person> persons = PersonList.getPersonList();
+        ArrayList<String> matrics = new ArrayList<>();
+
+        for (Person person : persons) {
+            matrics.add(person.getMatricNo());
+        }
+
+        for (String matric : matrics) {
+            PersonList.delete(matric);
+        }
+    }
 
     @Test
     void testExecuteAddPerson_ValidPerson_Succeeds() {
@@ -28,7 +50,6 @@ class AddPersonCommandTest {
         assertDoesNotThrow(() -> cmd.execute(testStockList, testCli, testStorage));
     }
 
-    //@@author cyanoei
     @Test
     void testExecuteAddPerson_RepeatedPerson_ThrowsBadInputException() {
         AddPersonCommand cmd =  new AddPersonCommand(CommandType.ADD, "A1", "John Doe");
