f80fbec81b21ce666fb8af6f06f344ec6b62b48a
Rebecca Oei <e0322808@u.nus.edu>
2019-11-11 17:18:18 +0800

    PersonList works now, thanks Dana

--- src/test/java/eggventory/logic/commands/add/AddLoanCommandTest.java
+++ src/test/java/eggventory/logic/commands/add/AddLoanCommandTest.java
@@ -4,12 +4,16 @@ import eggventory.commons.enums.CommandType;
 import eggventory.commons.exceptions.BadInputException;
 import eggventory.model.PersonList;
 import eggventory.model.StockList;
+import eggventory.model.loans.Person;
 import eggventory.storage.Storage;
 import eggventory.stubs.StorageStub;
 import eggventory.stubs.UiStub;
 import eggventory.ui.Ui;
 import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
+
+import java.util.ArrayList;
 
 //@@author cyanoei
 
@@ -26,9 +30,28 @@ class AddLoanCommandTest {
     private String name1 = "testName";
     private String stockDescription = "desc";
 
+    @BeforeEach
+    void resetPersonList() throws BadInputException {
+        if (PersonList.getSize() == 0) {
+            return;
+        }
+
+        ArrayList<Person> persons = PersonList.getPersonList();
+        ArrayList<String> matrics = new ArrayList<>();
+
+        for (Person person : persons) {
+            matrics.add(person.getMatricNo());
+        }
+
+        for (String matric : matrics) {
+            PersonList.delete(matric);
+        }
+    }
+
     @Test
     void testAddLoan_StockAndPersonExists_ExpectedOutput() throws BadInputException {
         list.addStock(stockType, stockCode, stockQuantity, stockDescription);
+        PersonList.add(matric1, name1);
 
         String output = new AddLoanCommand(CommandType.ADD, matric1, stockCode, 10)
                 .execute(list, testCli, testStorage);
@@ -40,6 +63,7 @@ class AddLoanCommandTest {
     @Test
     void testAddLoan_PersonDoesNotExist_OutputMessage() throws BadInputException {
         list.addStock(stockType, stockCode, stockQuantity, stockDescription);
+        PersonList.add(matric1, name1);
 
         String output = new AddLoanCommand(CommandType.ADD, "notMatric", stockCode, 10)
                 .execute(list, testCli, testStorage);
@@ -60,6 +84,7 @@ class AddLoanCommandTest {
     @Test
     void testAddLoan_QuantityInsufficient_OutputMessage() throws BadInputException {
         list.addStock(stockType, stockCode, stockQuantity, stockDescription);
+        PersonList.add(matric1, name1);
 
         String output = new AddLoanCommand(CommandType.ADD, matric1, stockCode, 1000)
                 .execute(list, testCli, testStorage);
